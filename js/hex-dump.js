615a7b601cf86ee4a87cdcc3f60672f7f21c7988
Sayapin Alexander <a.sayapin@a1-systems.com>
2018-02-05 15:58:34 +0300

    + Added hex dump styles

--- js/hex-dump.js
+++ js/hex-dump.js
@@ -11,9 +11,17 @@ HexDump.cleanup = function(hexString) {
 };
 
 HexDump.formatHexDump = function(element, hexString) {
-	
+	HexDump.formatOffsetPanel();
+	HexDump.formatDataPanel();
+	HexDump.formatCharacterPanel();
 };
 
 HexDump.formatOffsetPanel = function(hexString) {
 	
+};
+
+HexDump.formatOffsetCell = function(offsetValue) {
+	var hexValue = 0;	
+	
+	return "<span data-offset=\""+offsetValue+"\">"+hexValue+":"+offsetValue+"</span>";
 };
