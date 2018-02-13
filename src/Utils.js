aa35a9440b15603cec3fdd3ec348d5213da94aa9
Sayapin Alexander <a.sayapin@a1-systems.com>
2018-02-13 17:38:12 +0300

    * Fix

--- src/Utils.js
+++ src/Utils.js
@@ -1,4 +1,4 @@
-const Utils = {};
+var Utils = {};
 Utils.Text = {};
 Utils.Text.isPrint=function(val) {
 	if (val > 0x20 && val < 0x7f) {
@@ -82,8 +82,10 @@ Utils.Text.Encoding = {};
 
 Utils.Text.Encoding.GSM0338 = {};
 
-Utils.Text.Encoding.GSM0338.basicSet = "@Δ 0¡P¿p£_!1AQaq$Φ\"2BRbr¥Γ#3CScsèΛ¤4DTdtéΩ%5EUeuùΠ&6FVfvìΨ'7GWgwòΣ(8HXhxÇΘ)9IYiy\rΞ*:JZjzØ@+;KÄkäøÆ,<LÖlö\næ-=MÑmñÅß.>NÜnüåÉ/?O§oà";
+Utils.Text.Encoding.GSM0338.basicSet = "@£$¥èéùìòÇ\rØø\nÅåΔ_ΦΓΛΩΠΨΣΘΞ@ÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà";
 Utils.Text.Encoding.GSM0338.extendedSet = "|^€{}[~]\\";
+Utils.Text.Encoding.GSM0338.mapping = [0x40, 0x14, 0x65, 0x28, 0x29, 0x3c, 0x3d, 0x3e, 0x2f];
+Utils.Text.Encoding.GSM0338.escCharacter = 0x1B;
 
 Utils.Text.Encoding.GSM0338.isRepresentable = function(str) {
 	if (str == null || str.length == 0) {
@@ -95,7 +97,23 @@ Utils.Text.Encoding.GSM0338.isRepresentable = function(str) {
 			var res = Utils.Text.Encoding.GSM0338.isRepresentable(str.charCodeAt());
 		}
 	}
-}
+};
+
+Utils.Text.Encoding.GSM0338.isCharRepresentable = function(character) {
+	var basicPosition = Utils.Text.Encoding.GSM0338.basicSet.indexOf(character);
+
+	if (basicPosition >= 0) {
+        return true;
+    }
+
+    var extendedPosition = Utils.Text.Encoding.GSM0338.extendedSet.indexOf(character);
+
+	if (extendedPosition >= 0) {
+		return true;
+	}
+
+	return false;
+};
 
 Utils.Buffer = {};
 
@@ -170,4 +188,4 @@ console.log("Status:" + buf.readInt() +":" + buf.cap());
 console.log("Sequence number:"+buf.readInt() +":" + buf.cap());*/
 
 
-export {Utils}
+export default Utils;
